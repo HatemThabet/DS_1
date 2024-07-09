bf23b0f253ef31190d99d86e5ddd89a3e22dd947
unknown <hatemalashker1010@gmail.com>
2024-07-09 19:14:44 +0300

    first change

---
+++ product-price.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  8 13:52:11 2024
+
+@author: Hatem
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+pp=pd.read_csv('productprice.csv')
